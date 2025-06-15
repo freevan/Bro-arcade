ae601e5824d2db940548d53e7129dc60750dcb21
freevan <80598593+freevan@users.noreply.github.com>
2025-06-15 23:54:02 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,7 @@
+
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "#fff";
+ctx.font = "20px monospace";
+ctx.fillText("BRO ARCADE DEMO", 200, 200);
