6992920f68d0cc09fdaca8179186597738036d03
freevan <80598593+freevan@users.noreply.github.com>
2025-06-16 00:14:40 +0200

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,7 @@
+
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "white";
+ctx.font = "20px monospace";
+ctx.fillText("BRO ARCADE DEMO IS LIVE", 180, 200);
